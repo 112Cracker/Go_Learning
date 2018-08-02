df4c43af018a4d0df959e9666c292c40e072d73b
XuliangSun <xu94liang@gmail.com>
2018-08-02 16:47:30 -0400

    add notes on promotion

--- 20_struct/03_embedded-types/main.go
+++ 20_struct/03_embedded-types/main.go
@@ -30,3 +30,34 @@ func main() {
 	fmt.Printf("%s is a %d-year-old %s who worked in %s as a %s.\n",
 				person.name, person.age, person.gender, person.job.location, person.job.title)
 }
+/*
+(1) overriding fields in Go -> promotion!
+for example, the above employee rewrite as follows:
+
+type employee struct {
+	job job
+	name string
+	age int
+	gender string
+	location string
+}
+
+person := employee {
+	job: job {
+		title: "Manager"
+		id: 100
+		location: "Pittsburgh, PA"
+	},
+	name: "Tom"
+	age: 24,
+	gender: "Male"
+	location: "Cleveland"
+}
+location value in the outer scope override the inner scope location value
+
+(2) overriding methods in Go -> promotion!
+
+job could have a method called A
+employee could have a method called A as a promotion(overriding A belongs to job) to A belongs to job
+
+ */
